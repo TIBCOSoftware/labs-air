ae18214a9cf155655458f2c818b34647077c70c2
torresashjian <migueltorreslopez@gmail.com>
2019-10-08 14:23:21 -0700

    Add notifications gateway

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -4,8 +4,9 @@ function build_and_push(){
     image_name=$1
     image_tag=$2
     image_url=$3
+    docker_file=$4
     echo "Building image  ${image_name}..."
-    docker build -t ${image_name} .
+    docker build -t ${image_name} -f ${docker_file}
     echo "Tagging image ${image_url}/${image_name}:${image_tag}..."
     docker tag ${image_name} ${image_url}/${image_name}:${image_tag}
     echo "Pushing image ${image_url}/${image_name}:${image_tag}..."
