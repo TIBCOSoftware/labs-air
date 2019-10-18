8cac1e3a03b5d89b44c8567758b672722fd2bfdb
marcelo <gallardo@tibco.com>
2019-10-18 11:47:26 -0400

    Overwrote titles and highlights

--- ui/IoTCloudStarter/src/app/routes/splash/splash.component.ts
+++ ui/IoTCloudStarter/src/app/routes/splash/splash.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
-import {LandingPageItemConfig, TcCoreCommonFunctions} from '@tibco-tcstk/tc-core-lib';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LandingPageItemConfig, TcCoreCommonFunctions } from '@tibco-tcstk/tc-core-lib';
 import { Location } from '@angular/common';
 
 @Component({
@@ -15,6 +15,25 @@ export class SplashComponent implements OnInit {
   public highlights: LandingPageItemConfig[];
   public navigateURL: string;
 
+  private splashHighlights = [
+    {
+      "title": "Engage",
+      "iconURL": "ic-documentation",
+      "content": "Handle all IoT related requests in a single place - e.g. manage gateways, manage devices, view device data."
+    },
+    {
+      "title": "Collaborate",
+      "iconURL": "ic-community",
+      "content": "Collaborate on open and interoperable IoT solutions built on open standards"
+    },
+    {
+      "title": "Track",
+      "iconURL": "ic-graph",
+      "content": "Track and respond to high priority devices using alerts and actions"
+    }
+  ];
+
+
   constructor(private router: Router, private route: ActivatedRoute, private location: Location) {
   }
 
@@ -24,21 +43,42 @@ export class SplashComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    console.log("Landing Page Config: ", this.route.snapshot.data.landingPagesConfigHolder);
+
     const splash_config = this.route.snapshot.data.landingPagesConfigHolder.landingPage[0];
 
-    this.title = splash_config.title;
-    this.subTitle = splash_config.subtitle;
-    //   this.backgroundImage = TcCoreCommonFunctions.prepareUrlForStaticResource(this.location, splash_config.backgroundURL);
+    console.log("Splash config: ", splash_config);
+
+    this.title = "Welcome to Project AIR";
+    this.subTitle = "A customizable CloudStarter application to manage IoT assets";
+
+      // this.title = splash_config.title;
+      // this.subTitle = splash_config.subtitle;
+      // this.backgroundImage = TcCoreCommonFunctions.prepareUrlForStaticResource(this.location, splash_config.backgroundURL);
+
+
 
     this.highlights = new Array();
-    splash_config.highlights.forEach(highlight => {
-        this.highlights.push(new LandingPageItemConfig().deserialize({
-            title: highlight.title,
-            content: highlight.content,
-            iconURL: highlight.iconURL
-        }));
+    
+    // splash_config.highlights.forEach(highlight => {
+    //   this.highlights.push(new LandingPageItemConfig().deserialize({
+    //     title: highlight.title,
+    //     content: highlight.content,
+    //     iconURL: highlight.iconURL
+    //   }));
+
+    // });
+
+    this.splashHighlights.forEach(highlight => {
+      this.highlights.push(new LandingPageItemConfig().deserialize({
+        title: highlight.title,
+        content: highlight.content,
+        iconURL: highlight.iconURL
+      }));
 
     });
+
     this.navigateURL = splash_config.homeRoute;
   }
 
